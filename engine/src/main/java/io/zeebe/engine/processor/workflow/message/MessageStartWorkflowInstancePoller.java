38d78ea6eb30eb51906e0ec18a4d67043f4af34a
Philipp Ossler <philipp.ossler@gmail.com>
2019-11-20 13:32:24 +0100

    clean up

--- engine/src/main/java/io/zeebe/engine/processor/workflow/message/MessageStartWorkflowInstancePoller.java
+++ engine/src/main/java/io/zeebe/engine/processor/workflow/message/MessageStartWorkflowInstancePoller.java
@@ -11,12 +11,15 @@ import io.zeebe.engine.processor.KeyGenerator;
 import io.zeebe.engine.processor.workflow.BpmnStepContext;
 import io.zeebe.engine.processor.workflow.deployment.model.element.ExecutableFlowElementContainer;
 import io.zeebe.engine.processor.workflow.deployment.model.element.ExecutableStartEvent;
+import io.zeebe.engine.state.deployment.DeployedWorkflow;
+import io.zeebe.engine.state.message.Message;
 import io.zeebe.engine.state.message.MessageState;
 import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceRecord;
 import io.zeebe.protocol.record.intent.WorkflowInstanceIntent;
 import io.zeebe.protocol.record.value.BpmnElementType;
 import io.zeebe.util.sched.clock.ActorClock;
 import java.util.function.Consumer;
+import org.agrona.DirectBuffer;
 
 public class MessageStartWorkflowInstancePoller
     implements Consumer<BpmnStepContext<ExecutableFlowElementContainer>> {
@@ -24,13 +27,10 @@ public class MessageStartWorkflowInstancePoller
   private final MessageState messageState;
   private final KeyGenerator keyGenerator;
 
-  // private final MessageStartEventSubscriptionState subscriptionState;
-
   private final WorkflowInstanceRecord startEventRecord =
       new WorkflowInstanceRecord().setBpmnElementType(BpmnElementType.START_EVENT);
 
-  private long messageKeyToCorrelate;
-  private ExecutableStartEvent startEventToCorrelate;
+  private final Correlation messageCorrelation = new Correlation();
 
   public MessageStartWorkflowInstancePoller(
       final KeyGenerator keyGenerator, final MessageState messageState) {
@@ -41,86 +41,137 @@ public class MessageStartWorkflowInstancePoller
   @Override
   public void accept(final BpmnStepContext<ExecutableFlowElementContainer> context) {
 
-    messageKeyToCorrelate = Long.MAX_VALUE;
-    startEventToCorrelate = null;
-
-    final var bpmnProcessId = context.getValue().getBpmnProcessIdBuffer();
     final var workflowInstanceKey = context.getValue().getWorkflowInstanceKey();
     final var correlationKey = messageState.getWorkflowInstanceCorrelationKey(workflowInstanceKey);
 
     if (correlationKey != null) {
-
       messageState.removeWorkflowInstanceCorrelationKey(workflowInstanceKey);
 
-      final var deployedWorkflow =
-          context.getStateDb().getLatestWorkflowVersionByProcessId(bpmnProcessId);
-      final var workflow = deployedWorkflow.getWorkflow();
+      // the workflow instance was created by a message with a correlation key
+      // - other messages with same correlation key are not correlated to this workflow until this
+      // instance is ended (workflow-correlation-key lock)
+      // - now, after the instance is ended, correlate the next buffered message
+      correlateNextBufferedMessage(correlationKey, context);
+    }
+  }
 
-      for (final ExecutableStartEvent startEvent : workflow.getStartEvents()) {
-        if (startEvent.isMessage()) {
+  private void correlateNextBufferedMessage(
+      final DirectBuffer correlationKey,
+      final BpmnStepContext<ExecutableFlowElementContainer> context) {
 
-          messageState.visitMessages(
-              startEvent.getMessage().getMessageName(),
-              correlationKey,
-              message -> {
-                // correlate first message with same correlation key that was not correlated yet
-                if (message.getDeadline() > ActorClock.currentTimeMillis()
-                    && !messageState.existMessageCorrelation(message.getKey(), bpmnProcessId)) {
+    final var bpmnProcessId = context.getValue().getBpmnProcessIdBuffer();
+    final var workflow = context.getStateDb().getLatestWorkflowVersionByProcessId(bpmnProcessId);
 
-                  // correlate to the first message across all message start events
-                  // - using the message key to decide which message was correlated before
-                  if (message.getKey() < messageKeyToCorrelate) {
-                    messageKeyToCorrelate = message.getKey();
-                    startEventToCorrelate = startEvent;
-                  }
+    final var messageCorrelation = findNextMessageToCorrelate(workflow, correlationKey);
 
-                  return false;
+    if (messageCorrelation == null) {
+      // no buffered message to correlate
+      // - release the workflow-correlation-key lock
+      messageState.removeActiveWorkflowInstance(bpmnProcessId, correlationKey);
+
+    } else {
+      final var message = messageState.getMessage(messageCorrelation.messageKey);
+
+      correlateMessage(workflow, messageCorrelation.elementId, message, context);
+    }
+  }
+
+  private Correlation findNextMessageToCorrelate(
+      final DeployedWorkflow workflow, final DirectBuffer correlationKey) {
+
+    messageCorrelation.messageKey = Long.MAX_VALUE;
+    messageCorrelation.elementId = null;
+
+    for (final ExecutableStartEvent startEvent : workflow.getWorkflow().getStartEvents()) {
+      if (startEvent.isMessage()) {
+
+        final var messageName = startEvent.getMessage().getMessageName();
+
+        messageState.visitMessages(
+            messageName,
+            correlationKey,
+            message -> {
+              // correlate the first message with same correlation key that was not correlated yet
+              if (message.getDeadline() > ActorClock.currentTimeMillis()
+                  && !messageState.existMessageCorrelation(
+                      message.getKey(), workflow.getBpmnProcessId())) {
+
+                // correlate the first published message across all message start events
+                // - using the message key to decide which message was published before
+                if (message.getKey() < messageCorrelation.messageKey) {
+                  messageCorrelation.messageKey = message.getKey();
+                  messageCorrelation.elementId = startEvent.getId();
                 }
 
-                return true;
-              });
-        }
-      }
+                return false;
+              }
 
-      if (startEventToCorrelate == null) {
-        // no pending message to correlate
-        messageState.removeActiveWorkflowInstance(bpmnProcessId, correlationKey);
-
-      } else {
-        // correlate next pending message by creating a new workflow instance
-        final var message = messageState.getMessage(messageKeyToCorrelate);
-
-        final var workflowKey = deployedWorkflow.getKey();
-
-        // TODO (saig0): extract correlation logic from PublishMessageProcessor
-        final boolean wasTriggered =
-            context
-                .getStateDb()
-                .getEventScopeInstanceState()
-                .triggerEvent(
-                    workflowKey,
-                    message.getKey(),
-                    startEventToCorrelate.getId(),
-                    message.getVariables());
-
-        if (wasTriggered) {
-
-          final var newWorkflowInstanceKey = keyGenerator.nextKey();
-
-          context
-              .getOutput()
-              .appendFollowUpEvent(
-                  newWorkflowInstanceKey,
-                  WorkflowInstanceIntent.EVENT_OCCURRED,
-                  startEventRecord
-                      .setWorkflowKey(workflowKey)
-                      .setWorkflowInstanceKey(newWorkflowInstanceKey)
-                      .setElementId(startEventToCorrelate.getId()));
-
-          messageState.putMessageCorrelation(message.getKey(), bpmnProcessId);
-          messageState.putWorkflowInstanceCorrelationKey(newWorkflowInstanceKey, correlationKey);
-        }
+              return true;
+            });
       }
     }
+
+    return messageCorrelation.elementId != null ? messageCorrelation : null;
+  }
+
+  private void correlateMessage(
+      final DeployedWorkflow workflow,
+      final DirectBuffer elementId,
+      final Message message,
+      final BpmnStepContext<ExecutableFlowElementContainer> context) {
+
+    createEventTrigger(workflow, elementId, message, context);
+
+    final long workflowInstanceKey = createNewWorkflowInstance(workflow, elementId, context);
+
+    // mark the message as correlated
+    messageState.putMessageCorrelation(message.getKey(), workflow.getBpmnProcessId());
+    messageState.putWorkflowInstanceCorrelationKey(
+        workflowInstanceKey, message.getCorrelationKey());
+  }
+
+  private void createEventTrigger(
+      final DeployedWorkflow workflow,
+      final DirectBuffer elementId,
+      final Message message,
+      final BpmnStepContext<ExecutableFlowElementContainer> context) {
+
+    final boolean success =
+        context
+            .getStateDb()
+            .getEventScopeInstanceState()
+            .triggerEvent(workflow.getKey(), message.getKey(), elementId, message.getVariables());
+
+    if (!success) {
+      throw new IllegalStateException(
+          String.format(
+              "Expected the event trigger for be created of the workflow with key '%d' but failed.",
+              workflow.getKey()));
+    }
+  }
+
+  private long createNewWorkflowInstance(
+      final DeployedWorkflow workflow,
+      final DirectBuffer startEventElementId,
+      final BpmnStepContext<ExecutableFlowElementContainer> context) {
+
+    final var workflowInstanceKey = keyGenerator.nextKey();
+
+    context
+        .getOutput()
+        .appendFollowUpEvent(
+            workflowInstanceKey,
+            WorkflowInstanceIntent.EVENT_OCCURRED,
+            startEventRecord
+                .setWorkflowKey(workflow.getKey())
+                .setWorkflowInstanceKey(workflowInstanceKey)
+                .setElementId(startEventElementId));
+
+    return workflowInstanceKey;
+  }
+
+  private static class Correlation {
+    private long messageKey;
+    private DirectBuffer elementId;
   }
 }
